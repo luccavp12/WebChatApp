782438512b70a1a42f746a455d7719c55165c6dc
Lucca Psaila <lucca12vp@gmail.com>
2020-04-27 14:44:09 -0700

    Allowed use of seperate CSS file

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-var app = require('express')();
+var express = require('express'); 
+var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+
+app.use(express.static('.'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
